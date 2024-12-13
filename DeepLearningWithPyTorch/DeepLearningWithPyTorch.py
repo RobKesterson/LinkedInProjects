6ca65cda4f989b553dbe7256e406295ae9371f6f
Rob_Kesterson <Simon.Illyan@gmail.com>
2024-12-12 17:53:30 -0800

    Defined initial comments for PyTorch example. Now to finish since the library is closing.

---
+++ DeepLearningWithPyTorch/DeepLearningWithPyTorch.py
@@ -0,0 +1,16 @@
+"""
+Robert Kesterson
+
+Convolutional neural networks (CNNs) were all the rage at image classification challenges from about 2014 to 2017.
+They were bested by recurrent convolutional neural networks (RNNs) from 2017 to 2020.
+
+In 2017 a paper called "Attention is all you need" was published by Ashish Vaswani et. al which proposed the existence
+of transformer architectures. Transformers set the world of ai / ml alight and have been responsible for a significant
+amount of new developments in the field since 2020.
+
+What follows below is a humble example of simple CNNs trained against the CIFAR10 data set with the goal of
+arrurately classifying 60,000 images into 10 categories.
+"""
+
+
+
